d546ca8e05525d7cfe13a6ed087ec6fd033b8f8d
rodrigo.rodrigues <rodrigo.rodrigues0990@hotmail.com>
2026-08-16 14:54:19 -0300

    tela de loading

---
+++ javaScript/loading.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const splashScreen = document.getElementById('splash-screen');
+
+  if (splashScreen) {
+    setTimeout(() => {
+      splashScreen.classList.add('fade-out');
+
+      // Remove do DOM após a animação de fade-out ser concluída
+      setTimeout(() => {
+        splashScreen.remove();
+      }, 1000);
+    }, 2500);
+  }
+});
